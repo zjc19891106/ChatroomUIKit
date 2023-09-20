6488b53d2ee30f395794aab62946c57e35a37e52
朱继超 <984065974@qq.com>
2023-09-20 11:15:57 +0800

    Update ExampleRequiredConfig.swift

--- Example/ChatroomUIKit/ExampleRequiredConfig.swift
+++ Example/ChatroomUIKit/ExampleRequiredConfig.swift
@@ -15,16 +15,16 @@ import ChatroomUIKit
  **/
 public class ExampleRequiredConfig {
     // https://docs.agora.io/en/agora-chat/get-started/enable?platform=ios
-    static let appKey: String = "1182230915209398#chatroomuikit-test"
+    static let appKey: String = <#T##String#>
     //  ProjectManager->Operation Manager->User->Create User.Then Basic Information->Application Info->Chat User temp token.Next,fill below param.Move on,fill user id.
-    static let chatToken: String = "YWMtxgbdola8Ee6UGq1IimamaZGgQVxu0koxi-vGtfdnugRF312QU4sR7p1bEc5SYULxAwMAAAGKrE5MVDeeSAB1UEl0Bc9XdBkrsYRzBt9H-88RwQgDZyJc6YEmtvbSlg"
+    static let chatToken: String = <#T##String#>
     //    Follow the following process to create a chat room on console.
     //    ProjectManager->Operation Manager->Chat Room->Create Chat Room.Then fill in the `chatroomId` parameter below.
     
     /// `YourAppUser` means that you application project's user class.
     public final class YourAppUser: NSObject,UserInfoProtocol {
         
-        public var userId: String = "18811508760"
+        public var userId: String = <#T##String#>
         
         public var nickName: String = "Jack"
         
