6f7de0a3ffe50c887e15c05c883d787a45bc058c
朱继超 <984065974@qq.com>
2023-10-23 10:44:39 +0800

    update

--- Sources/ChatroomUIKit/Classes/UI/Components/Gift/Controllers/GiftsViewController.swift
+++ Sources/ChatroomUIKit/Classes/UI/Components/Gift/Controllers/GiftsViewController.swift
@@ -42,6 +42,10 @@ extension GiftsViewController: GiftsViewActionEventsDelegate {
     /// - Parameter item: `GiftEntityProtocol`
     open func onGiftSendClick(item: GiftEntityProtocol) {
         //It can be called after completing the interaction related to the gift sending interface with the server.
+        if ChatroomContext.shared?.muteMap?[ChatroomContext.shared?.currentUser?.userId ?? ""] ?? false {
+            self.showToast(toast: "Your were muted!".chatroom.localize, duration: 2)
+           return
+        }
         if item.sentThenClose {
             UIViewController.currentController?.dismiss(animated: true)
         }
