f181e6ac8648b542131605eac50fd50afbdc4b90
朱继超 <984065974@qq.com>
2023-12-08 14:36:33 +0800

    push

--- Sources/ChatroomUIKit/Classes/UI/Components/Chat/Controllers/ReportOptionsController.swift
+++ Sources/ChatroomUIKit/Classes/UI/Components/Chat/Controllers/ReportOptionsController.swift
@@ -36,7 +36,7 @@ import UIKit
         self.reportClosure = completion
         self.reportMessage = message
     }
-
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
         self.items = Appearance.reportTags.map({ $0 == "Adult" })
